7e51ac68bb8842d9c3665245af496f611659b723
Serhat Yurdakul <57943268+serhatyurdakul@users.noreply.github.com>
2025-12-31 12:49:37 +0300

    Add Inter Variable font and update base font family
    Added @fontsource-variable/inter to dependencies and imported it in main.jsx. Updated SCSS base font family to use 'Inter Variable' for improved typography consistency.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
+import "@fontsource-variable/inter";
 import "./styles/main.scss";
 import { store } from "./store";
 
